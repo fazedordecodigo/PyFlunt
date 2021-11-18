83b166c0f5a833c1255975c3d293a1ccbcb5a4b6
Emerson Delatorre <38289677+Delatorrea@users.noreply.github.com>
2021-11-18 11:02:30 -0300

    Criada classe Notification e adicioando docstring

---
+++ pyflunt/notification.py
@@ -0,0 +1,10 @@
+"""Module Notification."""
+
+
+class Notification:
+    """Class Notification."""
+
+    def __init__(self, key: str, message: str) -> None:
+        """Found 'Constructor'."""
+        self.key: str = key
+        self.message: str = message
