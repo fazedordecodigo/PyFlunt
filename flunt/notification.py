c7e65972423a63be35f5f5ed19c2631c38854f32
Emerson Delatorre <38289677+Delatorrea@users.noreply.github.com>
2021-11-19 16:53:56 -0300

    Refatorada classe Notification, campo key renomeado para Field e método __str__ criado.

--- flunt/notification.py
+++ flunt/notification.py
@@ -4,7 +4,10 @@
 class Notification:
     """Class Notification."""
 
-    def __init__(self, key: str, message: str) -> None:
+    def __init__(self, field: str, message: str) -> None:
         """Found 'Constructor'."""
-        self.key: str = key
+        self.field: str = field
         self.message: str = message
+
+    def __str__(self):
+        return '{}field: {}, message: {}{}'.format('{', self.field, self.message, '}')
