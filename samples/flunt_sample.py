a0078ee7307068457b40c9f3ac479fe5c861c74b
Emerson Delatorre <38289677+Delatorrea@users.noreply.github.com>
2021-11-19 16:56:10 -0300

    Corrigido sample

--- samples/flunt_sample.py
+++ samples/flunt_sample.py
@@ -1,26 +1,39 @@
+"""Module Value Objects."""
 from flunt.notifiable import Notifiable
 from flunt.contract import Contract
 
 
-class Email(Notifiable):
-    def __init__(self, email):
+class Name(Notifiable):
+    """Class Value Object Name."""
+
+    def __init__(self, first_name, last_name):
+        """Found 'Constructor'."""
         super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
 
-        self._email = email
-
-        contract = (
+        self.contract = (
             Contract()
-            .requires()
-            .has_max_len(
-                value=email,
-                maximum=50,
-                message='invalid email'
+            .requires(self.first_name, 'first name')
+            .requires(self.last_name, 'last name')
+            .has_min_len(
+                value=self.first_name,
+                minimum=3,
+                field='first_name',
+                message='invalid first name'
             )
-            .is_email(
-                value=email,
-                field='email',
-                message='invalid email'
+            .has_min_len(
+                value=self.last_name,
+                minimum=3,
+                field='last_name',
+                message='invalid last name'
             )
         )
 
-        self.add_notifications(contract)
+        self.add_notifications_of_contract(self.contract)
+
+
+nome = Name('Emerson', 'Delatorre')
+if not nome.is_valid():
+    for notification in nome.get_notifications():
+        print(notification)
