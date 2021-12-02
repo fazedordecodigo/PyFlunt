a11fefa78e1efa1253bbf859f81789ec1c22d911
Emerson Delatorre <38289677+Delatorrea@users.noreply.github.com>
2021-12-01 23:35:37 -0300

    Refatorado teste

--- tests/test_bool_validation.py
+++ tests/test_bool_validation.py
@@ -1,6 +1,7 @@
-from tests.entity.sample_entity import SampleEntity
-from flunt.validations.contract import Contract
 import pytest
+
+from flunt.validations.contract import Contract
+from tests.entity.sample_entity import SampleEntity
 
 
 @pytest.fixture
@@ -11,8 +12,8 @@ def _entity():
 def test_quando_bool_e_true_deve_ser_valido(_entity):
     contract = (
         Contract()
-        .requires(_entity.bool_true_property, 'Bool')
-        .is_true(_entity.bool_true_property, 'Bool', 'Custom message here')
+        .requires(_entity.bool_true_property, "Bool")
+        .is_true(_entity.bool_true_property, "Bool", "Custom message here")
     )
     assert contract.is_valid()
 
@@ -20,8 +21,8 @@ def test_quando_bool_e_true_deve_ser_valido(_entity):
 def test_quando_bool_e_false_deve_ser_valido(_entity):
     contract = (
         Contract()
-        .requires(_entity.bool_true_property, 'Bool')
-        .is_false(_entity.bool_false_property, 'Bool', 'Custom message here')
+        .requires(_entity.bool_true_property, "Bool")
+        .is_false(_entity.bool_false_property, "Bool", "Custom message here")
     )
     assert contract.is_valid()
 
@@ -29,8 +30,8 @@ def test_quando_bool_e_false_deve_ser_valido(_entity):
 def test_quando_bool_e_true_e_invalido_deve_conter_1_notificacao(_entity):
     contract = (
         Contract()
-        .requires(_entity.bool_true_property, 'Bool')
-        .is_true(_entity.bool_false_property, 'Bool', 'Custom message here')
+        .requires(_entity.bool_true_property, "Bool")
+        .is_true(_entity.bool_false_property, "Bool", "Custom message here")
     )
     assert len(contract.get_notifications()) == 1
 
@@ -38,8 +39,8 @@ def test_quando_bool_e_true_e_invalido_deve_conter_1_notificacao(_entity):
 def test_quando_bool_e_false_e_invalido_deve_conter_1_notificacao(_entity):
     contract = (
         Contract()
-        .requires(_entity.bool_true_property, 'Bool')
-        .is_false(_entity.bool_true_property, 'Bool', 'Custom message here')
+        .requires(_entity.bool_true_property, "Bool")
+        .is_false(_entity.bool_true_property, "Bool", "Custom message here")
     )
     assert len(contract.get_notifications()) == 1
 
@@ -47,8 +48,8 @@ def test_quando_bool_e_false_e_invalido_deve_conter_1_notificacao(_entity):
 def test_quando_bool_e_none_deve_ser_valido(_entity):
     contract = (
         Contract()
-        .requires(_entity.bool_true_property, 'Bool')
-        .is_none(_entity.bool_none_property, 'Bool', 'Custom message here')
+        .requires(_entity.bool_true_property, "Bool")
+        .is_none(_entity.bool_none_property, "Bool", "Custom message here")
     )
     assert contract.is_valid()
 
@@ -56,8 +57,8 @@ def test_quando_bool_e_none_deve_ser_valido(_entity):
 def test_quando_bool_e_none_e_invalido_deve_conter_1_notificacao(_entity):
     contract = (
         Contract()
-        .requires(_entity.bool_true_property, 'Bool')
-        .is_none(_entity.bool_true_property, 'Bool', 'Custom message here')
+        .requires(_entity.bool_true_property, "Bool")
+        .is_none(_entity.bool_true_property, "Bool", "Custom message here")
     )
     assert len(contract.get_notifications()) == 1
 
@@ -65,8 +66,8 @@ def test_quando_bool_e_none_e_invalido_deve_conter_1_notificacao(_entity):
 def test_quando_bool_nao_e_none_deve_ser_valido(_entity):
     contract = (
         Contract()
-        .requires(_entity.bool_true_property, 'Bool')
-        .is_not_none(_entity.bool_true_property, 'Bool', 'Custom message here')
+        .requires(_entity.bool_true_property, "Bool")
+        .is_not_none(_entity.bool_true_property, "Bool", "Custom message here")
     )
     assert contract.is_valid()
 
@@ -74,7 +75,7 @@ def test_quando_bool_nao_e_none_deve_ser_valido(_entity):
 def test_quando_bool_nao_e_none_e_invalido_deve_conter_1_notificacao(_entity):
     contract = (
         Contract()
-        .requires(_entity.bool_true_property, 'Bool')
-        .is_not_none(_entity.bool_none_property, 'Bool', 'Custom message here')
+        .requires(_entity.bool_true_property, "Bool")
+        .is_not_none(_entity.bool_none_property, "Bool", "Custom message here")
     )
     assert len(contract.get_notifications()) == 1
