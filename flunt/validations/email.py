97e4b12b45c745cca2a70f49ab23c044cfddd5ef
Emerson Delatorre <38289677+Delatorrea@users.noreply.github.com>
2021-12-11 00:53:48 -0300

    Criado modulo especializado em emails

---
+++ flunt/validations/email.py
@@ -0,0 +1,43 @@
+"""Module Contract."""
+import re
+
+from flunt.localization.flunt_regex_patterns import FluntRegexPatterns
+from flunt.notifications.notifiable import Notifiable
+from flunt.notifications.notification import Notification
+
+
+class EmailValidationContract(Notifiable):
+    def is_email(self, value: str, key: str, message: str):
+        """Requires a string is an email
+
+        :param value
+        :param key
+        :param message
+
+        :return
+        """
+        if not self._valid_email(value):
+            self.add_notification(Notification(key, message))
+
+        return self
+
+    def is_not_email(self, value: str, key: str, message: str):
+        """Requires a string is not an email
+
+        :param value
+        :param key
+        :param message
+
+        :return
+        """
+        if self._valid_email(value):
+            self.add_notification(Notification(key, message))
+
+        return self
+
+    def _valid_email(self, value):
+        return re.match(
+            FluntRegexPatterns().email_regex_pattern,
+            value,
+            re.IGNORECASE,
+        )
