1d76139d249be7188a97a87dd3df9444b5a62f98
Emerson Delatorre <emerson.delatorre@avanade.com>
2023-05-21 17:01:26 -0300

    🦄 refactor(regex):
    Refact regex cpf and cnpj

--- flunt/localization/flunt_regex_patterns.py
+++ flunt/localization/flunt_regex_patterns.py
@@ -5,14 +5,16 @@ REGEX_PASSPORT = "^(?!^0+$)[a-zA-Z0-9]{3,20}$"
 REGEX_ONLY_NUMBERS = r"[^0-9]+"
 REGEX_ONLY_LETTERS_AND_NUMBERS = r"[A-Za-z0-9_-]"
 REGEX_URL = r"^(http|https):(\/\/www\.|\/\/www\.|\/\/|\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$|(http|https):(\/\/localhost:\d*|\/\/127\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5]))(:[0-9]{1,5})?(\/.*)?$"
-REGEX_CPF_CNPJ = r"([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})"
+REGEX_CPF = r"^\d{3}\.?\d{3}\.?\d{3}-?\d{2}$"
+REGEX_CNPJ = r"^\d{2}\.?\d{3}\.?\d{3}/?\d{4}-?\d{2}$"
 
 
 class FluntRegexPatterns:
     """Class FluntRegexPatterns."""
 
     def __init__(self) -> None:
-        self.cpf_cnpj_regex_pattern = REGEX_CPF_CNPJ
+        self.cpf_regex_pattern = REGEX_CPF
+        self.cnpj_regex_pattern = REGEX_CNPJ
         self.email_regex_pattern = REGEX_EMAIL
         self.url_regex_pattern = REGEX_URL
         self.only_number_regex_pattern = REGEX_ONLY_NUMBERS
