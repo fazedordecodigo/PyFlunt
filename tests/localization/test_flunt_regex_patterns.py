14b62a2736b2420514c5405c02972557e8978d31
Emerson Delatorre <emerson.delatorre@avanade.com>
2023-05-21 17:02:20 -0300

    🧪 test(cpf and cnpj):
    Create tests to validation cpf and cnpj with regex

--- tests/localization/test_flunt_regex_patterns.py
+++ tests/localization/test_flunt_regex_patterns.py
@@ -15,23 +15,46 @@ def test_should_identify_a_invalid_email_address_returning_none(regex):
 
 def test_should_identify_a_valid_cpf_with_points_returning_not_none(regex):
     actual = "041.326.650-89"
-    expected = re.match(regex.cpf_cnpj_regex_pattern, actual)
+    expected = re.match(regex.cpf_regex_pattern, actual)
     assert expected is not None
 
 
 def test_should_identify_a_invalid_cpf_with_points_returning_none(regex):
     actual = "041.326.650-8"
-    expected = re.match(regex.cpf_cnpj_regex_pattern, actual)
+    expected = re.match(regex.cpf_regex_pattern, actual)
     assert expected is None
 
 
 def test_should_identify_a_valid_cpf_without_points_returning_not_none(regex):
     actual = "04132665089"
-    expected = re.match(regex.cpf_cnpj_regex_pattern, actual)
+    expected = re.match(regex.cpf_regex_pattern, actual)
     assert expected is not None
 
 
 def test_should_identify_a_invalid_cpf_without_points_returning_none(regex):
     actual = "0413266508"
-    expected = re.match(regex.cpf_cnpj_regex_pattern, actual)
+    expected = re.match(regex.cpf_regex_pattern, actual)
+    assert expected is None
+
+def test_should_identify_a_valid_cnpj_with_points_returning_not_none(regex):
+    actual = "26.710.845/0001-64"
+    expected = re.match(regex.cnpj_regex_pattern, actual)
+    assert expected is not None
+
+
+def test_should_identify_a_invalid_cnpj_with_points_returning_none(regex):
+    actual = "26.710.845/0001-6"
+    expected = re.match(regex.cnpj_regex_pattern, actual)
+    assert expected is None
+
+
+def test_should_identify_a_valid_cnpj_without_points_returning_not_none(regex):
+    actual = "26710845000164"
+    expected = re.match(regex.cnpj_regex_pattern, actual)
+    assert expected is not None
+
+
+def test_should_identify_a_invalid_cnpj_without_points_returning_none(regex):
+    actual = "2671084500016"
+    expected = re.match(regex.cnpj_regex_pattern, actual)
     assert expected is None
