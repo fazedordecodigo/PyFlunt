6197a35c152c90b4edbd79f9e9d7cdd031e5269b
Emerson Delatorre <emerson.delatorre@avanade.com>
2023-05-21 16:40:01 -0300

    🧪 test(cpf):
    Add test cpf with regex

--- tests/localization/test_flunt_regex_patterns.py
+++ tests/localization/test_flunt_regex_patterns.py
@@ -9,3 +9,23 @@ def test_should_identify_a_invalid_email_address_returning_none(regex):
     actual = 'any.com'
     expected = re.match(regex.email_regex_pattern, actual)
     assert expected is None
+
+def test_should_identify_a_valid_cpf_with_points_returning_not_none(regex):
+    actual = '041.326.650-89'
+    expected = re.match(regex.cpf_cnpj_regex_pattern, actual)
+    assert expected is not None
+
+def test_should_identify_a_invalid_cpf_with_points_returning_none(regex):
+    actual = '041.326.650-8'
+    expected = re.match(regex.cpf_cnpj_regex_pattern, actual)
+    assert expected is None
+
+def test_should_identify_a_valid_cpf_without_points_returning_not_none(regex):
+    actual = '04132665089'
+    expected = re.match(regex.cpf_cnpj_regex_pattern, actual)
+    assert expected is not None
+
+def test_should_identify_a_invalid_cpf_without_points_returning_none(regex):
+    actual = '0413266508'
+    expected = re.match(regex.cpf_cnpj_regex_pattern, actual)
+    assert expected is None
