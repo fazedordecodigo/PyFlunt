781c6ded1b128f04fa0282db00405f2e1d353b2a
Emerson Delatorre <emerson.delatorre@avanade.com>
2023-05-21 18:32:58 -0300

    🧪 test(string):
    create test is lower or equals then

--- tests/validations/test_string_validation_contract.py
+++ tests/validations/test_string_validation_contract.py
@@ -18,3 +18,21 @@ def test_should_be_invalid_and_return_once_notification_when_not_is_lower_then_5
     )
     assert contract.is_valid() is False
     assert len(contract.get_notifications()) == 1
+
+
+def test_should_be_valid_when_is_lower_or_equals_then_20(entityMock):
+    contract = (
+        Contract()
+        .is_lower_or_equals_than(entityMock.first_name, 20, "first_name", "any message")
+    )
+    assert contract.is_valid()
+    assert len(contract.get_notifications()) == 0
+
+
+def test_should_be_invalid_and_return_once_notification_when_not_is_lower_or_equals_then_14(entityMock):
+    contract = (
+        Contract()
+        .is_lower_or_equals_than(entityMock.first_name, 14, "first_name", "any message")
+    )
+    assert contract.is_valid() is False
+    assert len(contract.get_notifications()) == 1
