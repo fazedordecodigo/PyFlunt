53177506dc12ab7d1a63a4b20b2c1e4805fa0943
Emerson Delatorre <emerson.delatorre@avanade.com>
2023-05-21 18:02:01 -0300

    🦄 refactor(test email):

--- tests/mocks/entity/sample_entity.py
+++ tests/mocks/entity/sample_entity.py
@@ -10,3 +10,5 @@ class SampleEntity(Notification):
         self.bool_false_property = False
         self.bool_true_property = True
         self.bool_none_property = None
+        self.email_valid = "any@any.com"
+        self.email_invalid = "any"
