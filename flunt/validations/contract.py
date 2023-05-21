7ae621c98676755d81384bc5b32ab15453da516c
Emerson Delatorre <emerson.delatorre@avanade.com>
2023-05-21 17:46:01 -0300

    🦄 refactor(rename): email

--- flunt/validations/contract.py
+++ flunt/validations/contract.py
@@ -2,7 +2,7 @@
 from flunt.notifications.notifiable import Notifiable
 from flunt.notifications.notification import Notification
 from flunt.validations.bool_validation_contract import BoolValidationContract
-from flunt.validations.email import EmailValidationContract
+from flunt.validations.email_validation_contract import EmailValidationContract
 from flunt.validations.strings import StringValidationContract
 
 
