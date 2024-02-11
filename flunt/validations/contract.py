bb95a601c697f77960faed1f22cffcc34c7033b4
Emerson Delatorre <emerson.delatorre@avanade.com>
2024-02-11 17:15:16 -0300

    Add CreditCardValidationContract to Contract class

--- flunt/validations/contract.py
+++ flunt/validations/contract.py
@@ -2,6 +2,7 @@
 from flunt.notifications.notifiable import Notifiable
 from flunt.notifications.notification import Notification
 from flunt.validations.bool_validation_contract import BoolValidationContract
+from flunt.validations.credit_card_validation_contract import CreditCardValidationContract
 from flunt.validations.email_validation_contract import EmailValidationContract
 from flunt.validations.strings_validation_contract import StringValidationContract
 
@@ -10,6 +11,7 @@ class Contract(
     StringValidationContract,
     EmailValidationContract,
     BoolValidationContract,
+    CreditCardValidationContract,
     Notifiable,
 ):
     """Class Contract."""
