2843ef75733f4cc6467c3e939b6238780fa5980a
Emerson Delatorre <emerson.delatorre@avanade.com>
2024-02-11 17:17:21 -0300

    Remove unused imports and update sample_entity.py #10

--- tests/mocks/entity/sample_entity.py
+++ tests/mocks/entity/sample_entity.py
@@ -1,5 +1,4 @@
 """Module Sample Entity."""
-from faker import Faker
 from flunt.notifications.notification import Notification
 
 
@@ -8,13 +7,10 @@ class SampleEntity(Notification):
 
     def __init__(self) -> None:
         """Found 'Constructor'."""
-        fake = Faker()
         self.bool_false_property = False
         self.bool_true_property = True
         self.bool_none_property = None
-        self.email_valid = fake.email()
+        self.email_valid = "nome@emai.com"
         self.email_invalid = "any"
         self.first_name = "any_first_name"
         self.last_name = "any_last_name"
-        self.credit_card_valid = fake.credit_card_number(card_type='amex')
-        self.credit_card_invalid = "1234567890123456"
