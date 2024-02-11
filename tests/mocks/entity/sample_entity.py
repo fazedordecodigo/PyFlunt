512efd2f1327f8b6ad3ea150414cd842483adb9c
Emerson Delatorre <emerson.delatorre@avanade.com>
2024-02-11 17:15:20 -0300

    Add faker library for generating fake data in SampleEntity class

--- tests/mocks/entity/sample_entity.py
+++ tests/mocks/entity/sample_entity.py
@@ -1,4 +1,5 @@
 """Module Sample Entity."""
+from faker import Faker
 from flunt.notifications.notification import Notification
 
 
@@ -7,10 +8,13 @@ class SampleEntity(Notification):
 
     def __init__(self) -> None:
         """Found 'Constructor'."""
+        fake = Faker()
         self.bool_false_property = False
         self.bool_true_property = True
         self.bool_none_property = None
-        self.email_valid = "any@any.com"
+        self.email_valid = fake.email()
         self.email_invalid = "any"
         self.first_name = "any_first_name"
         self.last_name = "any_last_name"
+        self.credit_card_valid = fake.credit_card_number(card_type='amex')
+        self.credit_card_invalid = "1234567890123456"
