975b9defca5f3e8194c3da779e24e6ac0c0f2842
Emerson Delatorre <emerson.delatorre@avanade.com>
2024-03-01 16:26:59 -0300

    Refactor string validation contract #11

--- tests/validations/test_string_validation_contract.py
+++ tests/validations/test_string_validation_contract.py
@@ -1,9 +1,10 @@
-from flunt.validations.contract import Contract
+
+from flunt.validations.strings_validation_contract import StringValidationContract
 from tests.mocks.entity.sample_entity import SampleEntity
 
 
 def test_should_be_valid_when_is_lower_than_20(entity_mock: SampleEntity):
-	contract = Contract().is_lower_than(
+	contract = StringValidationContract().is_lower_than(
 		entity_mock.first_name, 20, 'first_name', 'any message'
 	)
 	assert contract.is_valid
@@ -13,7 +14,7 @@ def test_should_be_valid_when_is_lower_than_20(entity_mock: SampleEntity):
 def test_should_be_invalid_and_return_once_notification_when_not_is_lower_than_5(
 	entity_mock: SampleEntity,
 ):
-	contract = Contract().is_lower_than(
+	contract = StringValidationContract().is_lower_than(
 		entity_mock.first_name, 5, 'first_name', 'any message'
 	)
 	assert contract.is_valid is False
@@ -23,7 +24,7 @@ def test_should_be_invalid_and_return_once_notification_when_not_is_lower_than_5
 def test_should_be_valid_when_is_lower_or_equals_than_20(
 	entity_mock: SampleEntity,
 ):
-	contract = Contract().is_lower_or_equals_than(
+	contract = StringValidationContract().is_lower_or_equals_than(
 		entity_mock.first_name, 20, 'first_name', 'any message'
 	)
 	assert contract.is_valid
@@ -33,7 +34,7 @@ def test_should_be_valid_when_is_lower_or_equals_than_20(
 def test_should_be_invalid_and_return_once_notification_when_not_is_lower_or_equals_than_14(
 	entity_mock: SampleEntity,
 ):
-	contract = Contract().is_lower_or_equals_than(
+	contract = StringValidationContract().is_lower_or_equals_than(
 		entity_mock.first_name, 14, 'first_name', 'any message'
 	)
 	assert contract.is_valid is False
@@ -41,7 +42,7 @@ def test_should_be_invalid_and_return_once_notification_when_not_is_lower_or_equ
 
 
 def test_should_be_valid_when_is_greater_than_10(entity_mock: SampleEntity):
-	contract = Contract().is_greater_than(
+	contract = StringValidationContract().is_greater_than(
 		entity_mock.first_name, 10, 'first_name', 'any message'
 	)
 	assert contract.is_valid
@@ -51,7 +52,7 @@ def test_should_be_valid_when_is_greater_than_10(entity_mock: SampleEntity):
 def test_should_be_invalid_and_return_once_notification_when_not_is_greater_than_15(
 	entity_mock: SampleEntity,
 ):
-	contract = Contract().is_greater_than(
+	contract = StringValidationContract().is_greater_than(
 		entity_mock.first_name, 15, 'first_name', 'any message'
 	)
 	assert contract.is_valid is False
@@ -61,7 +62,7 @@ def test_should_be_invalid_and_return_once_notification_when_not_is_greater_than
 def test_should_be_valid_when_is_greater_or_equals_than_13(
 	entity_mock: SampleEntity,
 ):
-	contract = Contract().is_greater_or_equals_than(
+	contract = StringValidationContract().is_greater_or_equals_than(
 		entity_mock.first_name, 13, 'first_name', 'any message'
 	)
 	assert contract.is_valid
@@ -71,37 +72,9 @@ def test_should_be_valid_when_is_greater_or_equals_than_13(
 def test_should_be_invalid_and_return_once_notification_when_not_is_greater_or_equals_than_14(
 	entity_mock: SampleEntity,
 ):
-	contract = Contract().is_greater_or_equals_than(
+	contract = StringValidationContract().is_greater_or_equals_than(
 		entity_mock.first_name, 14, 'first_name', 'any message'
 	)
-	assert contract.is_valid is False
-	assert len(contract.get_notifications()) == 1
-
-
-def test_should_be_valid_when_is_none():
-	contract = Contract().is_none(None, 'first_name', 'any message')  # type: ignore
-	assert contract.is_valid
-	assert len(contract.get_notifications()) == 0
-
-
-def test_should_be_invalid_and_return_once_notification_when_not_is_none(
-	entity_mock: SampleEntity,
-):
-	contract = Contract().is_none(entity_mock.first_name, 'first_name', 'any message')
-	assert contract.is_valid is False
-	assert len(contract.get_notifications()) == 1
-
-
-def test_should_be_valid_when_is_not_none(entity_mock: SampleEntity):
-	contract = Contract().is_not_none(
-		entity_mock.first_name, 'first_name', 'any message'
-	)
-	assert contract.is_valid
-	assert len(contract.get_notifications()) == 0
-
-
-def test_should_be_invalid_and_return_once_notification_when_not_none():
-	contract = Contract().is_not_none(None, 'first_name', 'any message')  # type: ignore
 	assert contract.is_valid is False
 	assert len(contract.get_notifications()) == 1
 
@@ -109,7 +82,7 @@ def test_should_be_invalid_and_return_once_notification_when_not_none():
 def test_should_be_valid_when_is_not_none_or_white_space(
 	entity_mock: SampleEntity,
 ):
-	contract = Contract().is_not_none_or_white_space(
+	contract = StringValidationContract().is_not_none_or_white_space(
 		entity_mock.first_name, 'first_name', 'any message'
 	)
 	assert contract.is_valid
@@ -117,13 +90,13 @@ def test_should_be_valid_when_is_not_none_or_white_space(
 
 
 def test_should_be_invalid_and_return_once_notification_when_is_none():
-	contract = Contract().is_not_none_or_white_space(None, 'first_name', 'any message')  # type: ignore
+	contract = StringValidationContract().is_not_none_or_white_space(None, 'first_name', 'any message')  # type: ignore
 	assert contract.is_valid is False
 	assert len(contract.get_notifications()) == 1
 
 
 def test_should_be_invalid_and_return_once_notification_when_white_space():
-	contract = Contract().is_not_none_or_white_space(' ', 'first_name', 'any message')
+	contract = StringValidationContract().is_not_none_or_white_space(' ', 'first_name', 'any message')
 	assert contract.is_valid is False
 	assert len(contract.get_notifications()) == 1
 
