67850b14920a6264c88b909432b0506ee410c129
Emerson Delatorre <emerson.delatorre@avanade.com>
2024-03-01 21:57:09 -0300

    Refactor bool validation contract

--- tests/validations/test_bool_validation_contract.py
+++ tests/validations/test_bool_validation_contract.py
@@ -1,5 +1,6 @@
 import pytest
-from flunt.validations.contract import Contract
+
+from flunt.validations.bool_validation_contract import BoolValidationContract
 
 @pytest.fixture()
 def message():
@@ -8,8 +9,7 @@ def message():
 
 def test_should_be_valid_when_is_true(entity_mock, message):
 	contract = (
-		Contract()
-		.requires(entity_mock.bool_true_property, 'Bool', message)
+		BoolValidationContract()
 		.is_true(entity_mock.bool_true_property, 'Bool', message)
 	)
 	assert contract.is_valid
@@ -17,8 +17,7 @@ def test_should_be_valid_when_is_true(entity_mock, message):
 
 def test_should_be_valid_when_is_false(entity_mock, message):
 	contract = (
-		Contract()
-		.requires(entity_mock.bool_true_property, 'Bool', message)
+		BoolValidationContract()
 		.is_false(entity_mock.bool_false_property, 'Bool', message)
 	)
 	assert contract.is_valid
@@ -26,8 +25,7 @@ def test_should_be_valid_when_is_false(entity_mock, message):
 
 def test_should_return_a_once_notification_when_is_true_is_invalid(entity_mock, message):
 	contract = (
-		Contract()
-		.requires(entity_mock.bool_true_property, 'Bool', message)
+		BoolValidationContract()
 		.is_true(entity_mock.bool_false_property, 'Bool', message)
 	)
 	assert len(contract.get_notifications()) == 1
@@ -37,46 +35,7 @@ def test_should_return_a_once_notification_when_is_false_is_invalid(
 	entity_mock, message
 ):
 	contract = (
-		Contract()
-		.requires(entity_mock.bool_true_property, 'Bool', message)
+		BoolValidationContract()
 		.is_false(entity_mock.bool_true_property, 'Bool', message)
-	)
-	assert len(contract.get_notifications()) == 1
-
-
-def test_should_return_valid_when_is_none_is_valid(entity_mock, message):
-	contract = (
-		Contract()
-		.requires(entity_mock.bool_true_property, 'Bool', message)
-		.is_none(entity_mock.bool_none_property, 'Bool', message)
-	)
-	assert contract.is_valid
-
-
-def test_should_return_a_once_notification_when_is_none_is_invalid(entity_mock, message):
-	contract = (
-		Contract()
-		.requires(entity_mock.bool_true_property, 'Bool', message)
-		.is_none(entity_mock.bool_true_property, 'Bool', message)
-	)
-	assert len(contract.get_notifications()) == 1
-
-
-def test_should_return_valid_when_is_not_none_is_valid(entity_mock, message):
-	contract = (
-		Contract()
-		.requires(entity_mock.bool_true_property, 'Bool', message)
-		.is_not_none(entity_mock.bool_true_property, 'Bool', message)
-	)
-	assert contract.is_valid
-
-
-def test_should_return_a_once_notification_when_is_not_none_is_invalid(
-	entity_mock, message
-):
-	contract = (
-		Contract()
-		.requires(entity_mock.bool_true_property, 'Bool', message)
-		.is_not_none(entity_mock.bool_none_property, 'Bool', message)
 	)
 	assert len(contract.get_notifications()) == 1
