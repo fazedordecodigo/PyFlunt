2cf38eccd23d9e29a1cb7ebf951b85770c20732c
Emerson Delatorre <emerson.delatorre@avanade.com>
2024-03-02 20:54:55 -0300

    Add parameterized tests for contract validation #11

--- tests/validations/test_contract.py
+++ tests/validations/test_contract.py
@@ -1,4 +1,6 @@
 
+import pytest
+from uuid import uuid4
 from faker import Faker
 
 from flunt.validations.contract import Contract
@@ -6,6 +8,40 @@ from flunt.validations.contract import Contract
 
 fake = Faker()
 
-def should_be_valid_and_not_return_notification_when_required_is_not_none():
-    contract = Contract().requires(None, "any_key", "any_message")
+@pytest.mark.parametrize(
+	'input',
+	[
+		fake.uuid4(),
+		fake.date_object(),
+        fake.json(),
+        fake.pydecimal(),
+        fake.pydict(),
+        fake.pyfloat(),
+        fake.pyint(),
+        fake.pyiterable(),
+        fake.pylist(),
+        fake.pyset(),
+        fake.pystr(),
+        fake.pystr_format(),
+        fake.pystruct(),
+        fake.pytuple(),
+        True,
+        False,
+        uuid4()
+	],
+)
+def test_should_be_valid_and_not_return_notification_when_required_is_not_none(input):
+    contract = Contract().requires(input, fake.text(max_nb_chars=10), fake.text(max_nb_chars=20))
     assert contract.is_valid
+
+
+@pytest.mark.parametrize(
+	'input',
+	[
+		None,
+        '',
+	],
+)
+def test_should_be_invalid_and_return_once_notification_when_required_is_none_or_empty(input):
+    contract = Contract().requires(input, fake.text(max_nb_chars=10), fake.text(max_nb_chars=20))
+    assert contract.is_valid is False
