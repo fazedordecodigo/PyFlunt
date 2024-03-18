66f3ab6b65a8a0dd290121baea5183998676f5aa
Emerson Delatorre <emerson.delatorre@avanade.com>
2024-03-17 22:48:08 -0300

    Adiciona testes de validação para verificar se um valor está entre um intervalo específico #30

--- tests/validations/test_collections_validation_contract.py
+++ tests/validations/test_collections_validation_contract.py
@@ -19,6 +19,7 @@ def message():
 
 range_one_to_nineteen = randint(1, 19)
 range_twenty_to_fifty = randint(20, 50)
+range_fiftyone_to_onehundred = randint(51, 100)
 
 @pytest.mark.parametrize(
 	'input, expect',
@@ -208,35 +209,33 @@ def test_should_be_invalid_and_return_once_notification_when_not_is_greater_or_e
 	assert len(contract.get_notifications()) == 1
 
 
-def test_should_be_valid_and_not_return_notification_when_contains_a_search_string(entity_mock: SampleEntity, key, message):
-	contract = CollectionsValidationContract().contains(entity_mock.first_name, "any", key, message)
-	assert contract.is_valid
-	assert len(contract.get_notifications()) == 0
-
-
-def test_should_be_invalid_and_return_once_notification_when_not_contains_a_search_string(entity_mock: SampleEntity, key, message):
-	contract = CollectionsValidationContract().contains(entity_mock.first_name, "bla", key, message)
-	assert contract.is_valid is False
-	assert len(contract.get_notifications()) == 1
-
-
-def test_should_be_valid_and_not_return_notification_when_not_contains_a_search_string(entity_mock: SampleEntity, key, message):
-	contract = CollectionsValidationContract().not_contains(entity_mock.first_name, "bla", key, message)
-	assert contract.is_valid
-	assert len(contract.get_notifications()) == 0
-
-
-def test_should_be_invalid_and_return_once_notification_when_contains_a_search_string(entity_mock: SampleEntity, key, message):
-	contract = CollectionsValidationContract().not_contains(entity_mock.first_name, "any", key, message)
-	assert contract.is_valid is False
-	assert len(contract.get_notifications()) == 1
-
-
-def test_should_be_valid_and_not_return_notification_when_value_is_between():
-	contract = CollectionsValidationContract().is_between("any_text", 1, 10, "test", "any_message")
+@pytest.mark.parametrize(
+	'input, min, max',
+	[
+		(fake.pydict(nb_elements=range_one_to_nineteen, variable_nb_elements=False), range_one_to_nineteen, range_twenty_to_fifty),
+		(fake.pyiterable(nb_elements=range_one_to_nineteen, variable_nb_elements=False), range_one_to_nineteen, range_twenty_to_fifty),
+		(fake.pylist(nb_elements=range_one_to_nineteen, variable_nb_elements=False), range_one_to_nineteen, range_twenty_to_fifty),
+		(fake.pyset(nb_elements=range_one_to_nineteen, variable_nb_elements=False), range_one_to_nineteen, range_twenty_to_fifty),
+		(fake.pystr(max_chars=range_one_to_nineteen), range_one_to_nineteen, range_twenty_to_fifty),
+		(fake.pytuple(nb_elements=range_one_to_nineteen, variable_nb_elements=False), range_one_to_nineteen, range_twenty_to_fifty)
+	],
+)
+def test_should_be_valid_and_not_return_notification_when_value_is_between(input, min, max):
+	contract = CollectionsValidationContract().is_between(input, min, max, "test", "any_message")
 	assert contract.is_valid
 
 
-def test_should_be_invalid_and_return_once_notification_when_value_is_not_between():
-	contract = CollectionsValidationContract().is_between("any_text", 10, 20, "test", "any_message")
+@pytest.mark.parametrize(
+	'input, min, max',
+	[
+		(fake.pydict(nb_elements=range_one_to_nineteen, variable_nb_elements=False), range_twenty_to_fifty, range_fiftyone_to_onehundred),
+		(fake.pyiterable(nb_elements=range_one_to_nineteen, variable_nb_elements=False), range_twenty_to_fifty, range_fiftyone_to_onehundred),
+		(fake.pylist(nb_elements=range_one_to_nineteen, variable_nb_elements=False), range_twenty_to_fifty, range_fiftyone_to_onehundred),
+		(fake.pyset(nb_elements=range_one_to_nineteen, variable_nb_elements=False), range_twenty_to_fifty, range_fiftyone_to_onehundred),
+		(fake.pystr(max_chars=range_one_to_nineteen), range_twenty_to_fifty, range_fiftyone_to_onehundred),
+		(fake.pytuple(nb_elements=range_one_to_nineteen, variable_nb_elements=False), range_twenty_to_fifty, range_fiftyone_to_onehundred)
+	],
+)
+def test_should_be_invalid_and_return_once_notification_when_value_is_not_between(input, min, max):
+	contract = CollectionsValidationContract().is_between(input, min, max, "test", "any_message")
 	assert contract.is_valid is False
