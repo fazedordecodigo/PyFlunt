6eeb66c2658a10ec3515aba12cd80b043857c998
Emerson Delatorre <emerson.delatorre@avanade.com>
2024-05-01 21:20:33 -0300

    Atualizar contrato de validação de strings com novos métodos

--- flunt/validations/strings_validation_contract.py
+++ flunt/validations/strings_validation_contract.py
@@ -2,6 +2,11 @@
 
 from typing_extensions import Self
 
+from flunt.constants.messages import (
+	CONTAINS,
+	IS_NOT_NONE_OR_WHITESPACE,
+	NOT_CONTAINS,
+)
 from flunt.notifications.notifiable import Notifiable
 from flunt.notifications.notification import Notification
 
@@ -12,53 +17,55 @@ class StringValidationContract(Notifiable):
 
 	Parameters
 	----------
-	    N/A
+		N/A
 
-	Attributes:
+	Attributes
 	----------
-	    N/A
+		N/A
 
-	Methods:
+	Methods
 	-------
-	- is_not_none_or_white_space(value, key, message): Checks if a string value is not None or whitespace.
+	- is_not_none_or_white_space(value, field, message): Checks if a string value is not None or whitespace.
+	- contains(value, comparer, field, message): Checks if a string value contains another string.
+	- not_contains(value, comparer, field, message): Checks if a string value does not contain another string.
 
 	"""
 
-	def is_not_none_or_white_space(self, value: str, key: str, message: str) -> Self:
+	def is_not_none_or_white_space(self, value: str, field: str, message: str = IS_NOT_NONE_OR_WHITESPACE) -> Self:
 		"""
 		Check if a string value is not None or whitespace and adds a notification if it is.
 
 		Parameters
 		----------
 		`value`: str
-		    The string value to be checked.
-		`key`: str
-		    The key or identifier associated with the check.
-		`message`: str
-		    The notification message to be added if the value is None or whitespace.
+			The string value to be checked.
+		`field`: str
+			The field or identifier associated with the check.
+		`message`: str (optional)
+			The notification message to be added if the value is None or whitespace.
 
-		Returns:
+		Returns
 		-------
 		`self`
-		    The current instance with potential notifications added.
+			The current instance with potential notifications added.
 
-		Notes:
+		Notes
 		-----
 		- If the `value` is None or consists only of whitespace characters (spaces, tabs, newlines, etc.),
-		a notification is added to the current instance with the provided `key` and `message`.
+		a notification is added to the current instance with the provided `field` and `message`.
 		- If the `value` is not None and contains at least one non-whitespace character, no notification is added.
 
-		Examples:
+		Examples
 		--------
 		```python
 		obj = Contract()
-		      .is_not_none_or_white_space("Hello", "ValueCheck", "Value should not be None or whitespace")
+			.is_not_none_or_white_space("Hello", "ValueCheck", "Value should not be None or whitespace")
 		obj.is_valid
 		```
 
 		"""
 		if value is None or value.isspace():
-			self.add_notification(Notification(key, message))
+			self.add_notification(Notification(field, message.format(field)))
 
 		return self
 
@@ -67,8 +74,8 @@ class StringValidationContract(Notifiable):
 			self,
 			value: str,
 			comparer: str,
-			key: str,
-			message: str
+			field: str,
+			message: str = CONTAINS
 		) -> Self:
 		"""
 		Check if a string value contains another string and adds a notification if it does.
@@ -76,36 +83,36 @@ class StringValidationContract(Notifiable):
 		Parameters
 		----------
 		`value`: str
-		    The string value to be checked.
+			The string value to be checked.
 		`comparer`: str
-		    The string to search for within the value.
-		`key`: str
-		    The key or identifier associated with the check.
-		`message`: str
-		    The notification message to be added if the value contains the comparer.
+			The string to search for within the value.
+		`field`: str
+			The field or identifier associated with the check.
+		`message`: str (optional)
+			The notification message to be added if the value contains the comparer.
 
-		Returns:
+		Returns
 		-------
 		`self`
-		    The current instance with potential notifications added.
+			The current instance with potential notifications added.
 
-		Notes:
+		Notes
 		-----
 		- If the `value` contains the `comparer` string, a notification is added to the current instance
-		with the provided `key` and `message`.
+		with the provided `field` and `message`.
 		- If the `value` does not contain the `comparer` string, no notification is added.
 
-		Examples:
+		Examples
 		--------
 		```python
 		obj = Contract()
-		      .contains("Hello, world!", "world", "ContainsCheck", "Value should contain 'world'")
+			.contains("Hello, world!", "world", "ContainsCheck", "Value should contain 'world'")
 		obj.is_valid
 		```
 
 		"""
 		if value.find(comparer) == -1:
-			self.add_notification(Notification(key, message))
+			self.add_notification(Notification(field, message.format(field, comparer)))
 			return self
 
 		return self
@@ -115,8 +122,8 @@ class StringValidationContract(Notifiable):
 			self,
 			value: str,
 			comparer:str,
-			key: str,
-			message: str
+			field: str,
+			message: str = NOT_CONTAINS
 		) -> Self:
 		"""
 		Check if a string value does not contain a specified string and adds a notification if it does.
@@ -124,35 +131,35 @@ class StringValidationContract(Notifiable):
 		Parameters
 		----------
 		`value`: str
-		    The string value to be checked.
+			The string value to be checked.
 		`comparer`: str
-		    The string to search for in the value.
-		`key`: str
-		    The key or identifier associated with the check.
-		`message`: str
-		    The notification message to be added if the value contains the comparer.
+			The string to search for in the value.
+		`field`: str
+			The field or identifier associated with the check.
+		`message`: str (optional)
+			The notification message to be added if the value contains the comparer.
 
-		Returns:
+		Returns
 		-------
 		`self`
-		    The current instance with potential notifications added.
+			The current instance with potential notifications added.
 
-		Notes:
+		Notes
 		-----
 		- If the `value` does not contain the `comparer` string, no notification is added.
 		- If the `value` contains the `comparer` string, a notification is added to the current instance
-		with the provided `key` and `message`.
+		with the provided `field` and `message`.
 
-		Examples:
+		Examples
 		--------
 		```python
 		obj = Contract()
-		      .not_contains("Hello", "World", "Comparison", "Value should not contain 'World'")
+			.not_contains("Hello", "World", "Comparison", "Value should not contain 'World'")
 		obj.is_valid
 		```
 
 		"""
 		if value.find(comparer) != -1:
-			self.add_notification(Notification(key, message))
+			self.add_notification(Notification(field, message.format(field, comparer)))
 			return self
 		return self
