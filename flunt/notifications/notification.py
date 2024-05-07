8c85841299ff0ceb7afb8cc72f6faea14e315f5a
Emerson Delatorre <emerson.delatorre@avanade.com>
2024-05-06 23:04:20 -0300

    refactor(dataclass): Transformado em dataclass

--- flunt/notifications/notification.py
+++ flunt/notifications/notification.py
@@ -1,13 +1,11 @@
 """Module Notification."""
 
+from dataclasses import dataclass
+
+
+@dataclass
 class Notification:
-	"""Class Notification."""
+    """Class Notification."""
 
-	def __init__(self, field: str, message: str) -> None:
-		"""Found 'Constructor'."""
-		self.field: str = field
-		self.message: str = message
-
-	def __repr__(self):
-		"""Representation object string."""
-		return f"{{ field: {self.field}, message: {self.message} }}"
+    field: str
+    message: str
