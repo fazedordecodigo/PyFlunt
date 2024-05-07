5246659ce8b6e75305217493efc7386ab61599b8
Emerson Delatorre <emerson.delatorre@avanade.com>
2024-05-06 23:02:38 -0300

    feat(Mensagem): Adicionado mensagem padrão

--- flunt/validations/strings_validation_contract.py
+++ flunt/validations/strings_validation_contract.py
@@ -3,163 +3,154 @@
 from typing_extensions import Self
 
 from flunt.constants.messages import (
-	CONTAINS,
-	IS_NOT_NONE_OR_WHITESPACE,
-	NOT_CONTAINS,
+    CONTAINS,
+    IS_NOT_NONE_OR_WHITESPACE,
+    NOT_CONTAINS,
 )
 from flunt.notifications.notifiable import Notifiable
-from flunt.notifications.notification import Notification
 
 
 class StringValidationContract(Notifiable):
-	"""
-	Class for validating string values and adding notifications based on various comparisons.
+    """
+    Class for validating string values and adding notifications based on various comparisons.
 
-	Parameters
-	----------
-		N/A
+    Parameters
+    ----------
+            N/A
 
-	Attributes
-	----------
-		N/A
+    Attributes
+    ----------
+            N/A
 
-	Methods
-	-------
-	- is_not_none_or_white_space(value, field, message): Checks if a string value is not None or whitespace.
-	- contains(value, comparer, field, message): Checks if a string value contains another string.
-	- not_contains(value, comparer, field, message): Checks if a string value does not contain another string.
+    Methods
+    -------
+    - is_not_none_or_white_space(value, field, message): Checks if a string value is not None or whitespace.
+    - contains(value, comparer, field, message): Checks if a string value contains another string.
+    - not_contains(value, comparer, field, message): Checks if a string value does not contain another string.
 
-	"""
+    """
 
-	def is_not_none_or_white_space(self, value: str, field: str, message: str = IS_NOT_NONE_OR_WHITESPACE) -> Self:
-		"""
-		Check if a string value is not None or whitespace and adds a notification if it is.
+    def is_not_none_or_white_space(
+        self, value: str, field: str, message: str = IS_NOT_NONE_OR_WHITESPACE
+    ) -> Self:
+        """
+        Check if a string value is not None or whitespace and adds a notification if it is.
 
-		Parameters
-		----------
-		`value`: str
-			The string value to be checked.
-		`field`: str
-			The field or identifier associated with the check.
-		`message`: str (optional)
-			The notification message to be added if the value is None or whitespace.
+        Parameters
+        ----------
+        `value`: str
+                The string value to be checked.
+        `field`: str
+                The field or identifier associated with the check.
+        `message`: str (optional)
+                The notification message to be added if the value is None or whitespace.
 
-		Returns
-		-------
-		`self`
-			The current instance with potential notifications added.
+        Returns
+        -------
+        `self`
+                The current instance with potential notifications added.
 
-		Notes
-		-----
-		- If the `value` is None or consists only of whitespace characters (spaces, tabs, newlines, etc.),
-		a notification is added to the current instance with the provided `field` and `message`.
-		- If the `value` is not None and contains at least one non-whitespace character, no notification is added.
+        Notes
+        -----
+        - If the `value` is None or consists only of whitespace characters (spaces, tabs, newlines, etc.),
+        a notification is added to the current instance with the provided `field` and `message`.
+        - If the `value` is not None and contains at least one non-whitespace character, no notification is added.
 
-		Examples
-		--------
-		```python
-		obj = Contract()
-			.is_not_none_or_white_space("Hello", "ValueCheck", "Value should not be None or whitespace")
-		obj.is_valid
-		```
+        Examples
+        --------
+        ```python
+        obj = Contract()
+                .is_not_none_or_white_space("Hello", "ValueCheck", "Value should not be None or whitespace")
+        obj.is_valid
+        ```
 
-		"""
-		if value is None or value.isspace():
-			self.add_notification(Notification(field, message.format(field)))
+        """
+        if value is None or value.isspace():
+            self.add_notification(field, message.format(field))
+        return self
 
-		return self
+    def contains(
+        self, value: str, comparer: str, field: str, message: str = CONTAINS
+    ) -> Self:
+        """
+        Check if a string value contains another string and adds a notification if it does.
 
+        Parameters
+        ----------
+        `value`: str
+                The string value to be checked.
+        `comparer`: str
+                The string to search for within the value.
+        `field`: str
+                The field or identifier associated with the check.
+        `message`: str (optional)
+                The notification message to be added if the value contains the comparer.
 
-	def contains(
-			self,
-			value: str,
-			comparer: str,
-			field: str,
-			message: str = CONTAINS
-		) -> Self:
-		"""
-		Check if a string value contains another string and adds a notification if it does.
+        Returns
+        -------
+        `self`
+                The current instance with potential notifications added.
 
-		Parameters
-		----------
-		`value`: str
-			The string value to be checked.
-		`comparer`: str
-			The string to search for within the value.
-		`field`: str
-			The field or identifier associated with the check.
-		`message`: str (optional)
-			The notification message to be added if the value contains the comparer.
+        Notes
+        -----
+        - If the `value` contains the `comparer` string, a notification is added to the current instance
+        with the provided `field` and `message`.
+        - If the `value` does not contain the `comparer` string, no notification is added.
 
-		Returns
-		-------
-		`self`
-			The current instance with potential notifications added.
+        Examples
+        --------
+        ```python
+        obj = Contract()
+                .contains("Hello, world!", "world", "ContainsCheck", "Value should contain 'world'")
+        obj.is_valid
+        ```
 
-		Notes
-		-----
-		- If the `value` contains the `comparer` string, a notification is added to the current instance
-		with the provided `field` and `message`.
-		- If the `value` does not contain the `comparer` string, no notification is added.
+        """
+        if value.find(comparer) == -1:
+            self.add_notification(field, message.format(field, comparer))
+        return self
 
-		Examples
-		--------
-		```python
-		obj = Contract()
-			.contains("Hello, world!", "world", "ContainsCheck", "Value should contain 'world'")
-		obj.is_valid
-		```
+    def not_contains(
+        self,
+        value: str,
+        comparer: str,
+        field: str,
+        message: str = NOT_CONTAINS,
+    ) -> Self:
+        """
+        Check if a string value does not contain a specified string and adds a notification if it does.
 
-		"""
-		if value.find(comparer) == -1:
-			self.add_notification(Notification(field, message.format(field, comparer)))
-			return self
+        Parameters
+        ----------
+        `value`: str
+                The string value to be checked.
+        `comparer`: str
+                The string to search for in the value.
+        `field`: str
+                The field or identifier associated with the check.
+        `message`: str (optional)
+                The notification message to be added if the value contains the comparer.
 
-		return self
+        Returns
+        -------
+        `self`
+                The current instance with potential notifications added.
 
+        Notes
+        -----
+        - If the `value` does not contain the `comparer` string, no notification is added.
+        - If the `value` contains the `comparer` string, a notification is added to the current instance
+        with the provided `field` and `message`.
 
-	def not_contains(
-			self,
-			value: str,
-			comparer:str,
-			field: str,
-			message: str = NOT_CONTAINS
-		) -> Self:
-		"""
-		Check if a string value does not contain a specified string and adds a notification if it does.
+        Examples
+        --------
+        ```python
+        obj = Contract()
+                .not_contains("Hello", "World", "Comparison", "Value should not contain 'World'")
+        obj.is_valid
+        ```
 
-		Parameters
-		----------
-		`value`: str
-			The string value to be checked.
-		`comparer`: str
-			The string to search for in the value.
-		`field`: str
-			The field or identifier associated with the check.
-		`message`: str (optional)
-			The notification message to be added if the value contains the comparer.
-
-		Returns
-		-------
-		`self`
-			The current instance with potential notifications added.
-
-		Notes
-		-----
-		- If the `value` does not contain the `comparer` string, no notification is added.
-		- If the `value` contains the `comparer` string, a notification is added to the current instance
-		with the provided `field` and `message`.
-
-		Examples
-		--------
-		```python
-		obj = Contract()
-			.not_contains("Hello", "World", "Comparison", "Value should not contain 'World'")
-		obj.is_valid
-		```
-
-		"""
-		if value.find(comparer) != -1:
-			self.add_notification(Notification(field, message.format(field, comparer)))
-			return self
-		return self
+        """
+        if value.find(comparer) != -1:
+            self.add_notification(field, message.format(field, comparer))
+        return self
