7f5729acb9b5f08e0a9b4367d9cb4bf548b3d40f
Emerson Delatorre <38289677+fazedordecodigo@users.noreply.github.com>
2024-11-16 20:55:48 -0300

    Adiciona teste para mensagem padrão quando is_false é inválido

--- tests/validations/test_bool_validation_contract.py
+++ tests/validations/test_bool_validation_contract.py
@@ -4,7 +4,7 @@ from typing import TYPE_CHECKING
 
 import pytest
 
-from flunt.constants.messages import IS_TRUE
+from flunt.constants.messages import IS_FALSE, IS_TRUE
 from flunt.validations.bool_validation_contract import BoolValidationContract
 
 if TYPE_CHECKING:
@@ -59,3 +59,12 @@ def test_should_return_a_standard_message_when_is_true_is_invalid(
         entity_mock.bool_false_property, "Bool"
     )
     assert contract.get_notifications()[0].message == IS_TRUE
+
+
+def test_should_return_a_standard_message_when_is_false_is_invalid(
+    entity_mock: SampleEntity,
+) -> None:
+    contract = BoolValidationContract().is_false(
+        entity_mock.bool_true_property, "Bool"
+    )
+    assert contract.get_notifications()[0].message == IS_FALSE
